2c19756a59182055560d176051f1d4681cac8964
Mikita <nikita_popow_1993@mail.ru>
2024-01-19 15:43:52 +0300

    delete trashe file

--- test.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-let calendar = Calendar.current
-let yest = calendar.date(byAdding: .day, value: -1, to: Date())
-let df = DateFormatter()
-df.dateFormat = "YYYY-MM-dd"
-
-print(yest)
