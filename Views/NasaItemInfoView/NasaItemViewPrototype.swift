3d9c9b565292d72996be16e06283a65d0dc48411
Mikita <nikita_popow_1993@mail.ru>
2024-01-22 14:07:12 +0300

    add action button

--- Views/NasaItemInfoView/NasaItemViewPrototype.swift
+++ Views/NasaItemInfoView/NasaItemViewPrototype.swift
@@ -20,6 +20,7 @@ struct NasaItemInfo: View {
     
     var body: some View {
         VStack{
+            //MARK: - Image
             AsyncImage(url: URL(string: item.url)) { im in
                 im
                     .resizable()
@@ -37,48 +38,88 @@ struct NasaItemInfo: View {
             .shadow(color: .black ,radius: 5)
             .padding(.top)
             
-            
+            //MARK: - date and action
             HStack{
                 Text(item.date ?? "").font(.system(size: 12))
                     .foregroundStyle(.secondary)
                 
                 Spacer()
                 
-                Image(systemName: DataManager.shared.isContainEl(item) ? "heart.fill" : "heart")
-                    .font(.system(size: 20))
-                    .foregroundStyle(DataManager.shared.isContainEl(item) ? .red : (colSch == .light ? .black : .white))
-                    .onTapGesture {
-                        DataManager.shared.addPic(item: item, context: context)
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                            isFavorite.toggle()
-                        }
-                    }
-                Button(action: {
-                    if let hdUrl = item.hdUrl{
-                        guard let url = URL(string: hdUrl) else { return }
-                        UIApplication.shared.open(url)
-                    }else{
-                        guard let url = URL(string: item.url) else { return }
-                        UIApplication.shared.open(url)
-                        
-                    }
-                }, label: {
-                    Image(systemName: "square.and.arrow.down")
-                        .font(.system(size: 20))
-                })
+//                Image(systemName: DataManager.shared.isContainEl(item) ? "heart.fill" : "heart")
+//                    .font(.system(size: 20))
+//                    .foregroundStyle(DataManager.shared.isContainEl(item) ? .red : (colSch == .light ? .black : .white))
+//                    .onTapGesture {
+//                        DataManager.shared.addPic(item: item, context: context)
+//                        withAnimation(.easeInOut(duration: 0.3)) {
+//                            isFavorite.toggle()
+//                        }
+//                    }
+//                Button(action: {
+//                    if let hdUrl = item.hdUrl{
+//                        guard let url = URL(string: hdUrl) else { return }
+//                        UIApplication.shared.open(url)
+//                    }else{
+//                        guard let url = URL(string: item.url) else { return }
+//                        UIApplication.shared.open(url)
+//                        
+//                    }
+//                }, label: {
+//                    Image(systemName: "square.and.arrow.down")
+//                        .font(.system(size: 20))
+//                })
             }
             .padding(.bottom)
             .foregroundStyle(colSch == .light ? .black : .white)
             
+            GeometryReader{size in
+                HStack{
+                    Button(action: {
+                        DataManager.shared.addPic(item: item, context: context)
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            isFavorite.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: DataManager.shared.isContainEl(item) ? "heart.fill" : "heart")
+                            .font(.system(size: 25))
+                            .foregroundStyle(DataManager.shared.isContainEl(item) ? .red : (colSch == .light ? .black : .white))
+                    })
+                    .frame(width: size.size.width / 2, height: size.size.height * 0.9)
+                    .background(LinearGradient(colors: [
+                        Color.blue, Color.gray, Color.blue
+                    ]
+                                               , startPoint: .topLeading,
+                                               endPoint: .bottomTrailing).opacity(0.3))
+                    .clipShape(.capsule)
+                    
+                    Spacer()
+                    Button(action: {
+                        if let hdUrl = item.hdUrl{
+                            guard let url = URL(string: hdUrl) else { return }
+                            UIApplication.shared.open(url)
+                        }else{
+                            guard let url = URL(string: item.url) else { return }
+                            UIApplication.shared.open(url)
+                            
+                        }
+                    }, label: {
+                        Image(systemName: "arrow.down.circle")
+                            .font(.system(size: 25))
+                    })
+                    .frame(width: size.size.width / 2, height: size.size.height * 0.9)
+                    .background(LinearGradient(colors: [
+                        Color.blue, Color.gray, Color.blue
+                    ]
+                                               , startPoint: .topLeading,
+                                               endPoint: .bottomTrailing).opacity(0.3))
+                    .clipShape(.capsule)
+
+                }
+            }.frame(height: 40)
+            
+            //MARK: - Title
             HStack {
                 Text(item.title ?? "")
                     .font(.system(.title2))
-                Button(action: {
-                    self.isShowDetails.toggle()
-                }, label: {
-                    Image(systemName: "info.circle")
-                        .foregroundStyle(.blue)
-                })
                 Spacer()
             }
             .sheet(isPresented: $isShowDetails){
@@ -96,14 +137,27 @@ struct NasaItemInfo: View {
             }
             Divider()
             
-            Text(item.descr ?? "")
-                .lineLimit(4)
+            //MARK: - Description and full information
+            HStack (alignment: .lastTextBaseline){
+                Text(item.descr ?? "")
+                    .lineLimit(4)
                 .padding(.top)
+                Button(action: {
+                    self.isShowDetails.toggle()
+                }, label: {
+                    Image(systemName: "info.circle")
+                        .foregroundStyle(.blue)
+                })
+            }
             
             Spacer()
         }
         .navigationTitle("Information")
         .padding(.horizontal)
     }
+}
+
+#Preview {
+    ContentView()
 }
 
